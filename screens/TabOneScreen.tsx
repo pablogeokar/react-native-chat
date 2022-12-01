46b34defe2261c5b6b521d3fd75648cbbdee3fd2
Pablo George <pablogeokar@hotmail.com>
2022-12-01 18:49:10 -0300

    iniciando o desenvolvimento

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,53 @@
-import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { View, Image, Text, StyleSheet } from 'react-native'
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Image
+        source={{
+          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'
+        }}
+        style={styles.image}
+      />
+      <View style={styles.rightContainer}>
+        <View style={styles.row}>
+          <Text style={styles.name}>Elon Musk</Text>
+          <Text style={styles.text}>11:11 AM</Text>
+        </View>
+        <Text numberOfLines={1} style={styles.text}>Hla Hola coca cola dsfaf fdsafa fdasf fdsafafa fadsfafda dsafadfadf fdsafafa</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    padding: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  rightContainer: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 30,
+    marginRight: 10
+  },
+  name: {
     fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 3
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+  text: {
+    color: 'grey'
+  }
+})
+
